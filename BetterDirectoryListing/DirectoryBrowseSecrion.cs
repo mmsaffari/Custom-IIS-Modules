8ed0db8d32225277585b17c525935bcba6771202
Mohammad Mahdi Saffari <mmsaffari@gmail.com>
2021-03-01 09:12:51 +0330

    Revert "Going Home 3"
    This reverts commit 4b94ae69647b1568ea72251c51223c2468ed1867.

---
+++ BetterDirectoryListing/DirectoryBrowseSecrion.cs
@@ -0,0 +1,23 @@
+﻿using System.Configuration;
+
+namespace MMS.BetterDirectoryListing {
+	class DirectoryBrowseSection: ConfigurationSection {
+		public const bool DefaultEnabled = true;
+		public const string ConfigurationSectionName = "directoryBrowsing";
+
+		private static ConfigurationPropertyCollection _properties = new ConfigurationPropertyCollection();
+		private static readonly ConfigurationProperty _propEnabled = new ConfigurationProperty("enabled", typeof(bool), true);
+
+		static DirectoryBrowseSection() {
+			_properties.Add(_propEnabled);
+		}
+
+		protected override ConfigurationPropertyCollection Properties => _properties;
+
+		[ConfigurationProperty("enabled", DefaultValue = true)]
+		public bool Enabled {
+			get => (bool)base[_propEnabled];
+			set => base[_propEnabled] = value;
+		}
+	}
+}
--- TemporaryProject/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TemporaryProject.Global" Language="C#" %>
